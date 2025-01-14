074016a530a8d3bdad1f6221319064c6fb465095
Anna Sheshshtakauskas <annasheshtakauskas@gmail.com>
2025-01-15 00:53:59 +0300

    Добавление Функции для проверки четности числа

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 console.log('Hello, World')
+
+// Функция для проверки четности числа
+function checkEvenOdd() {
+    // Запросить у пользователя ввод числа
+    let userInput = prompt("Введите любое число:");
+
+    // Преобразовать ввод в число
+    let number = parseInt(userInput);
+
+    // Проверка на корректность ввода
+    if (isNaN(number)) {
+        alert("Пожалуйста, введите действительное число.");
+        return;
+    }
+
+    // Проверка четности
+    if (number % 2 === 0) {
+        alert(`${number} является четным числом.`);
+    } else {
+        alert(`${number} является нечетным числом.`);
+    }
+}
+
+// Вызов функции
+checkEvenOdd();
